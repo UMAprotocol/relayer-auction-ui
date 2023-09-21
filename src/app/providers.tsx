780be7f7fb8ab0ddc3ab9b0530f99712763b6a96
ryanwolhuter <dev@ryanwolhuter.com>
2023-09-21 13:51:48 +0200

    add chain libs
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

---
+++ src/app/providers.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import {
+  RainbowKitProvider,
+  connectorsForWallets,
+  getDefaultWallets,
+} from "@rainbow-me/rainbowkit";
+import {
+  argentWallet,
+  ledgerWallet,
+  trustWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+import { useEffect, useState } from "react";
+import { WagmiConfig, configureChains, createConfig } from "wagmi";
+import { goerli, mainnet } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [
+    mainnet,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [goerli] : []),
+  ],
+  [publicProvider()],
+);
+
+const projectId = "YOUR_PROJECT_ID";
+
+const { wallets } = getDefaultWallets({
+  appName: "APP_NAME",
+  projectId,
+  chains,
+});
+
+const demoAppInfo = {
+  appName: "APP_NAME",
+};
+
+const connectors = connectorsForWallets([
+  ...wallets,
+  {
+    groupName: "Other",
+    wallets: [
+      argentWallet({ projectId, chains }),
+      trustWallet({ projectId, chains }),
+      ledgerWallet({ projectId, chains }),
+    ],
+  },
+]);
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+  webSocketPublicClient,
+});
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains} appInfo={demoAppInfo}>
+        {mounted && children}
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
