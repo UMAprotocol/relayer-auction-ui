e3632555b20a5ae71de6c0628b6f7a2a6b8f73bb
ryanwolhuter <dev@ryanwolhuter.com>
2023-09-27 14:10:54 +0200

    add table style
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/app/components/Auctions/components/AuctionTable.tsx
+++ src/app/components/Auctions/components/AuctionTable.tsx
@@ -1,54 +1,72 @@
 import { Auction } from "@/types";
 
 export function AuctionTable(props: Auction) {
+  const isComplete = props.status === "complete";
+  const titleColor = isComplete ? "text-green-600" : "text-gray-500";
   return (
-    <>
-      <table>
-        <thead>
-          <tr>
-            <th colSpan={2}>
-              Auction ID: {props.auctionId} | Status: {props.status}
-            </th>
-          </tr>
-        </thead>
+    <div>
+      <h2 className="pl-5 text-2xl font-medium">
+        Auction ID: <span className={titleColor}>{props.auctionId}</span> |
+        Status:{" "}
+        <span className={titleColor}>{camelToTitleCase(props.status)}</span>
+      </h2>
+      <table className="w-full text-left text-sm text-gray-600">
+        <caption className="p-5 text-left text-lg font-semibold text-gray-900">
+          Deposit
+        </caption>
         <tbody>
-          {props.bids.map((bid, index) => (
-            <tr key={index}>
-              <td>Bid</td>
-              <td>Relayer Address: {bid.relayerAddress}</td>
+          {Object.entries(props.deposit).map(([key, value], index) => (
+            <tr
+              key={index}
+              className={index % 2 === 0 ? "bg-white" : "bg-gray-50"}
+            >
+              <th
+                scope="row"
+                className="whitespace-nowrap py-1 font-medium text-gray-700"
+              >
+                <td className="px-6 py-4">
+                  <strong className="font-semibold">
+                    {camelToTitleCase(key)}
+                  </strong>
+                </td>
+              </th>
+              <td className="px-2 py-4">{value}</td>
             </tr>
           ))}
         </tbody>
       </table>
-
-      <table>
-        <thead>
+      <table className="w-full text-left text-sm text-gray-600">
+        <caption className="bg-white p-5 text-left text-lg font-semibold text-gray-900 ">
+          Bids
+        </caption>
+        <thead className="bg-gray-50 text-xs uppercase text-gray-700">
           <tr>
-            <th>Recipient</th>
-            <th>Token Address</th>
-            <th>Amount</th>
-            <th>Destination Chain Id</th>
-            <th>Relayer Fee Pct</th>
-            <th>Quote Timestamp</th>
-            <th>Message</th>
-            <th>Max Count</th>
-            <th>Tx Value</th>
+            <th scope="col" className="px-6 py-3">
+              Relayer Address
+            </th>
+            <th>Winner</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>{props.deposit.recipient}</td>
-            <td>{props.deposit.tokenAddress}</td>
-            <td>{props.deposit.amount}</td>
-            <td>{props.deposit.destinationChainId}</td>
-            <td>{props.deposit.relayerFeePct}%</td>
-            <td>{props.deposit.quoteTimestamp}</td>
-            <td>{props.deposit.message}</td>
-            <td>{props.deposit.maxCount}</td>
-            <td>{props.deposit.txValue}</td>
-          </tr>
+          {props.bids.map((bid, index) => (
+            <tr key={index} className="border-b bg-white">
+              <td className="px-6 py-4">{bid.relayerAddress}</td>
+              <td className={`px-6 py-4 ${titleColor}`}>
+                {isComplete &&
+                  props.winningRelayer === bid.relayerAddress &&
+                  "✔"}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
-    </>
+    </div>
   );
+}
+
+function camelToTitleCase(str: string): string {
+  // Replace camelCase with space, then replace the first character of each word to uppercase
+  return str
+    .replace(/([a-z])([A-Z])/g, "$1 $2") // Add a space between words
+    .replace(/\b([a-z])/g, (char) => char.toUpperCase()); // Capitalize the first letter of each word
 }
